c84e4fa560758ba649e5a303f2ac19ec8e160d63
es-house <e.sapio64@gmail.com>
2025-06-17 16:39:53 +0200

    Added a simple animation to open and close the door. Linked open/close logic to the PressureButton.

--- Assets/Scripts/Interactables/DoorInteractable.cs
+++ Assets/Scripts/Interactables/DoorInteractable.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class DoorInteractable : MonoBehaviour
 {
+    [SerializeField]
+    private Animator animator;
     public void OpenDoor()
     {
         print("opening door");
+        animator.SetBool("isOpen", true);
     }
 
     public void CloseDoor()
     {
         print("closing door");
+        animator.SetBool("isOpen", false);
     }
 }
