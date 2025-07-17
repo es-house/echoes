d70c73ac885fae60118b816c3ee5b67c24252e90
es-house <e.sapio64@gmail.com>
2025-07-17 15:15:15 +0200

    Fixed audio on Goal problem while destroying the goal itself.

--- Assets/Scripts/Collectibles/CollectGoal.cs
+++ Assets/Scripts/Collectibles/CollectGoal.cs
@@ -1,21 +1,30 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
-[RequireComponent(typeof(AudioSource))]
 public class CollectGoal : MonoBehaviour
 {
-
+    [SerializeField]
+    private AudioClip audioClip;
     void OnTriggerEnter(Collider other)
     {
         print("triggering goal");
         if (other.CompareTag("Player"))
         {
             print("is player");
-            AudioSource audioSource = GetComponent<AudioSource>();
-            audioSource.Play();
-
-            Destroy(gameObject, .3f);
+            Play2DAudio(audioClip);
+            Destroy(gameObject, .2f);
         }
+    }
+
+    private void Play2DAudio(AudioClip audioClipToPlay) {
+        GameObject audio = new GameObject("Audio");
+        AudioSource audioSource = audio.AddComponent<AudioSource>();
+        audioSource.clip = audioClipToPlay;
+        audioSource.spatialBlend = 0f;
+        audioSource.volume = .6f;
+        audioSource.Play();
+
+        Destroy(audio, audioClipToPlay.length);
     }
 
 }
